93c8350f67ad0e54c9dead82bcd5991065619cff
Tarako0329 <86461908+Tarako0329@users.noreply.github.com>
2024-03-28 19:01:54 +0900

    ver1.11.0:クレジット決済(stripe-connect)ID登録

--- ajax_create_stripe_return_url.php
+++ ajax_create_stripe_return_url.php
@@ -15,6 +15,16 @@
 
 			log_writer2("\$account",$account,"lv3");
 
+			if(empty($account->settings->payments->statement_descriptor)){
+				header("Location:".ROOT_URL.'settlement.php?key='.$_GET["hash"]."&stripe_setting=unable");
+				exit();
+		
+			}else{
+				header("Location:".ROOT_URL.'settlement.php?key='.$_GET["hash"]."&stripe_setting=able");
+				exit();
+		
+			}
+
 			/*
 			$sql = "update Users_online set stripe_id = '".$id."' where uid = ".$_SESSION["user_id"];
 			$stmt = $pdo_h->prepare( $sql );
@@ -25,14 +35,12 @@
 
 			$alert_status = "success";
 		}catch(Exception $e){
-			log_writer2("\$e",$e->error,"lv3");
+			log_writer2("\$e",$e,"lv3");
 			$alert_status = "danger";
-			$error = $e->error;
+			header("Location:".ROOT_URL.'settlement.php?key='.$_GET["hash"]."&stripe_setting=unable");
+			exit();
 		}
 		
-		header("Location:".ROOT_URL.'settlement.php?key='.$_GET["hash"]."&stripe_setting=able");
-		exit();
 				
 	}
-  exit();
 ?>
--- ajax_del_stripeid.php
+++ ajax_del_stripeid.php
@@ -3,6 +3,9 @@
 require "php_header.php";
 $stripe = new \Stripe\StripeClient(S_KEY);
 
-$stripe->accounts->delete('acct_1Oy7ftEJy7v7RoQL', []);
-echo "削除しました";
+//$stripe->accounts->delete('acct_1OybDqAQ64vUhFxt', []);
+$account = $stripe->accounts->retrieve('acct_1OzF6iEA6e52aUl3', []);
+echo $account->settings->payments->statement_descriptor;
+echo "<br>削除しました<br>";
+echo $account;
 ?>
