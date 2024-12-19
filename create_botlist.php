fe72b4b64994c4473d36355e4ee1e9dbbcbb0de3
Tarako0329 <86461908+Tarako0329@users.noreply.github.com>
2024-12-19 14:27:48 +0900

    ver1.33.4:軽微な修正

--- create_botlist.php
+++ create_botlist.php
@@ -1,5 +1,28 @@
 <?php
 
 //botリストの作成
-file_put_contents("bot_list.txt",file_get_contents('https://raw.githubusercontent.com/monperrus/crawler-user-agents/master/crawler-user-agents.json'));
+// jsonファイルを落としてくる
+$json = file_get_contents('https://raw.githubusercontent.com/monperrus/crawler-user-agents/master/crawler-user-agents.json');
+// 落とすのに失敗したら終了
+if ($json === false) {
+    return;
+}
+
+// jsonを配列に変換
+$arr = json_decode($json, true);
+// 変換できなかったら終了
+if ($arr === null) {
+    return;
+}
+
+// 正規表現のパターンをまとめた配列を作る
+$pattern_list = [];
+foreach ($arr as $key => $value) {
+    $pattern_list[] = $value['pattern'];
+}
+
+// パターン配列を「|」で連結した文字列に変換
+$pattern_list_string = implode('|', $pattern_list);
+
+file_put_contents("bot_list.txt",$pattern_list_string);
 ?>
