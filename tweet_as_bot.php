e436039fe2ef5824e11c73249bd85594558aeeb8
Tarako0329 <86461908+Tarako0329@users.noreply.github.com>
2025-03-27 16:29:41 +0900

    ver1.36.3:SNS自動投稿機能

--- tweet_as_bot.php
+++ tweet_as_bot.php
@@ -1,8 +1,11 @@
 <?php
 //１日５０ツイートまでかつ月１５００ツイートまで
 if (php_sapi_name() != 'cli') {
-  //exit('このスクリプトはCLIからのみ実行可能です。');
+	exit('このスクリプトはCLIからのみ実行可能です。');
 }
+chdir(__DIR__);
+echo "処理を開始します。\n";
+
 require "php_header.php";
 register_shutdown_function('shutdown_ajax',basename(__FILE__));
 //log_writer2("\$_POST",$_POST,"lv1");
@@ -16,103 +19,142 @@ $status="false";
 //実行時間check
 $stmt = $pdo_h->prepare("select * from online_shop_config");
 $stmt->execute();
-$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$online_shop_config = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-if($row["next_post_time"] >= date('Y-m-d H:i:s')){
-  //実行時間前
-  //exit();
+if($online_shop_config[0]["next_post_time"] >= date('Y-m-d H:i:s')){
+	//実行時間前
+	exit();
 }
 
 if(EXEC_MODE==="Local"){
-    $msg = "ツイートが送信されました！";
-    $status = "success";
+		echo "ツイートが送信されました！";
+		$status = "success";
 }else{
-  //ポスト内容取得
+	//ポスト内容取得
 
-  //商品選定
-  $stmt = $pdo_h->prepare("select U.yagou,M.* from shouhinMS_online as M inner join Users_online as U on M.uid=U.uid where status='show' and IFNULL(auto_post_sns,'') not like '%X%' order by shouhinCD");
-  $stmt->execute();
-  $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  $post_index = rand(0,(count($row)-1));
-  
-  echo "<p>対象商品:".$row[$post_index]["shouhinNM"]."</p>";
-  //log_writer2("\$post_index",$post_index,"lv1");
-  //log_writer2("\$row",$row[$post_index],"lv1");
+	//商品選定
+	$stmt = $pdo_h->prepare("select U.yagou,M.* from shouhinMS_online as M inner join Users_online as U on M.uid=U.uid where status='show' and IFNULL(auto_post_sns,'') not like '%X%' order by shouhinCD");
+	$stmt->execute();
+	$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	if(count($row)===0){//全件投稿完了・フラグリセット
+		try{
+			$pdo_h->beginTransaction();
+			$stmt = $pdo_h->prepare("update shouhinMS_online set auto_post_sns=''");
+			$stmt->execute();
+			$pdo_h->commit();
+			echo "投稿済みフラグをリセット";
 
-  //つぶやき作成
-  $sns_type = "X" ;
-  $uid = $row[$post_index]["uid"];
-  $shouhinCD = $row[$post_index]["shouhinCD"];
-  $yagou = $row[$post_index]["yagou"];
-  $hinmei = $row[$post_index]["shouhinNM"];
-  $sort_info = $row[$post_index]["short_info"];
-  $information = $row[$post_index]["infomation"];
-  
-  $discription = "URL：".ROOT_URL."product.php?id=".$uid."-".$shouhinCD."&z=".$sns_type." 販売元:".$yagou." 商品名：".$hinmei."。説明：".$sort_info." ".$information;
-  
-  
-  $client = new Client(GEMINI);
-  
-  $response = $client->withV1BetaVersion()
-      ->generativeModel(ModelName::GEMINI_1_5_FLASH)
-      ->withSystemInstruction('凄腕インフルエンサーとして')
-      ->generateContent(
-          new TextPart($sns_type.'で購買意欲を掻き立てる日本語の投稿例を10個出力。'.$sns_type.'にそのまま投稿できるようにＵＲＬとハッシュタグも含めて作成。phpのjson_decodeで処理できるように[{"post":投稿例},{"post":投稿例}]で出力。JSONオブジェクトを、プレーンテキスト形式で出力してください'.$discription),
-      );
-      
-  //print nl2br($response->text());
-      
-  $answer = $response->text();
-  $answer = str_replace('```json','',$answer);
-  $answer = str_replace('```','',$answer);
-  $answer = str_replace('\n','',$answer);
-  $answer = str_replace('\r','',$answer);
-  $answer = str_replace('\r\n','',$answer);
-  $answer = substr($answer,1);
-  $answer = json_decode($answer,true);
-  //print_r($answer);
-  log_writer2("\$answer",$answer,"lv1");
-  
-  
-  $text = $answer[rand(0,9)]["post"];
-  
-  
-  define("API_KEY",$_ENV["X_API_KEY"]);
-  define("API_SECRET_KEY",$_ENV["X_API_SECRET_KEY"]);
-  define("ACCESS_TOKEN",$_ENV["X_ACCESS_TOKEN"]);
-  define("SECRET_ACCESS_TOKEN",$_ENV["X_SECRET_ACCESS_TOKEN"]);
-  
-  try{
-    $connection = new TwitterOAuth(
-      API_KEY,
-      API_SECRET_KEY,
-      ACCESS_TOKEN,
-      SECRET_ACCESS_TOKEN
-    );
-    
-    $connection->setApiVersion('2');
-    
-    //URLが半角23文字扱い。ハッシュタグは含まない
-    $result = $connection->post("tweets", ["text"=>$text], ['jsonPayload'=>true]);
-  
-    $httpCode = $connection->getLastHttpCode();
-    
-    echo $text."\n";
-    if ($httpCode == 201) { // 201は作成成功を示すステータスコード
-      //$this->info("ツイートが送信されました！");
-      echo "ツイートが送信されました！\n";
-      $status = "success";
-    } else {
-      $errorMessage = isset($result->errors) ?json_encode($result->errors, JSON_UNESCAPED_UNICODE) :'不明なエラー';
-      //$this->error("ツイートの送信に失敗しました。HTTPコード: $httpCode, エラーメッセージ: $errorMessage");
-      echo "ツイートの送信に失敗しました。HTTPコード: $httpCode, エラーメッセージ: $errorMessage ";
-      log_writer2("\$msg",$msg,"lv1");
-    }
-  }catch(Exception $e){
-    //print_r($e,true);
-    echo "catch(Exception \$e)";
-    log_writer2("\$e",$e,"lv0");
-  }
+			//商品再選定
+			$stmt = $pdo_h->prepare("select U.yagou,M.* from shouhinMS_online as M inner join Users_online as U on M.uid=U.uid where status='show' and IFNULL(auto_post_sns,'') not like '%X%' order by shouhinCD");
+			$stmt->execute();
+			$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		}catch(Exception $e){
+			$pdo_h->rollBack();
+			log_writer2("\$e",$e,"lv0");
+			echo "投稿済みフラグをリセットでエラー";
+		}
+	
+	}
+
+	$post_index = rand(0,(count($row)-1));
+	
+	echo "<p>対象商品:".$row[$post_index]["shouhinNM"]."</p>";
+	//log_writer2("\$post_index",$post_index,"lv1");
+	//log_writer2("\$row",$row[$post_index],"lv1");
+
+	//つぶやき作成
+	$sns_type = "X" ;
+	$uid = $row[$post_index]["uid"];
+	$shouhinCD = $row[$post_index]["shouhinCD"];
+	$yagou = $row[$post_index]["yagou"];
+	$hinmei = $row[$post_index]["shouhinNM"];
+	$sort_info = $row[$post_index]["short_info"];
+	$information = $row[$post_index]["infomation"];
+	
+	$discription = "URL：".ROOT_URL."product.php?id=".$uid."-".$shouhinCD."&z=".$sns_type." 販売元:".$yagou." 商品名：".$hinmei."。説明：".$sort_info." ".$information;
+	
+	
+	$client = new Client(GEMINI);
+	
+	$response = $client->withV1BetaVersion()
+			->generativeModel(ModelName::GEMINI_1_5_FLASH)
+			->withSystemInstruction('凄腕インフルエンサーとして')
+			->generateContent(
+					new TextPart($sns_type.'で購買意欲を掻き立てる日本語の投稿例を10個出力。'.$sns_type.'にそのまま投稿できるようにＵＲＬとハッシュタグも含めて作成。phpのjson_decodeで処理できるように[{"post":投稿例},{"post":投稿例}]で出力。URLとハッシュタグを除いた文字数は100文字以下。JSONオブジェクトを、プレーンテキスト形式で出力してください'.$discription),
+			);
+			
+	//print nl2br($response->text());
+			
+	$answer = $response->text();
+	$answer = str_replace('```json','',$answer);
+	$answer = str_replace('```','',$answer);
+	$answer = str_replace('\n','',$answer);
+	$answer = str_replace('\r','',$answer);
+	$answer = str_replace('\r\n','',$answer);
+	$answer = substr($answer,1);
+	$answer = json_decode($answer,true);
+	//print_r($answer);
+	log_writer2("\$answer",$answer,"lv1");
+	
+	
+	$text = $answer[rand(0,9)]["post"];
+	echo $text."<br>\n";
+	
+	define("API_KEY",$_ENV["X_API_KEY"]);
+	define("API_SECRET_KEY",$_ENV["X_API_SECRET_KEY"]);
+	define("ACCESS_TOKEN",$_ENV["X_ACCESS_TOKEN"]);
+	define("SECRET_ACCESS_TOKEN",$_ENV["X_SECRET_ACCESS_TOKEN"]);
+	
+	try{
+		$connection = new TwitterOAuth(
+			API_KEY,
+			API_SECRET_KEY,
+			ACCESS_TOKEN,
+			SECRET_ACCESS_TOKEN
+		);
+		
+		$connection->setApiVersion('2');
+		
+		//URLが半角23文字扱い。ハッシュタグは含まない
+		//$result = $connection->post("tweets", ["text"=>$text], ['jsonPayload'=>true]);
+		//$httpCode = $connection->getLastHttpCode();
+		$httpCode = 201;	//テスト運用・ツイートはしない
+		
+		
+		if ($httpCode == 201) { // 201は作成成功を示すステータスコード
+			//$this->info("ツイートが送信されました！");
+			echo "ツイートが送信されました！<br>\n";
+			$status = "success";
+			$stmt = $pdo_h->prepare("update shouhinMS_online set auto_post_sns='X' where uid=".$uid." and shouhinCD=".$shouhinCD);
+			$stmt->execute();
+	
+		} else {
+			$errorMessage = isset($result->errors) ?json_encode($result->errors, JSON_UNESCAPED_UNICODE) :'不明なエラー';
+			echo "ツイートの送信に失敗しました。<br>\n HTTPコード: $httpCode,<br>\n エラーメッセージ: $errorMessage ";
+			//log_writer2("\$msg",$msg,"lv1");
+		}
+	}catch(Exception $e){
+		//print_r($e,true);
+		echo "catch(Exception \$e)";
+		log_writer2("\$e",$e,"lv0");
+		exit();
+	}
+}
+
+if($status==="success"){//次回の実行時間をセット
+	try{
+		$pdo_h->beginTransaction();
+		$next = rand($online_shop_config[0]["post_interval_F"],$online_shop_config[0]["post_interval_T"]);
+		$stmt = $pdo_h->prepare("update online_shop_config set next_post_time=DATE_ADD(NOW(), INTERVAL ".$next." MINUTE)");
+		$stmt->execute();
+		$pdo_h->commit();
+		echo "次は ".$next." 分後です";
+	}catch(Exception $e){
+		$pdo_h->rollBack();
+		log_writer2("\$e",$e,"lv0");
+		echo "次の投稿時間設定でエラー";
+	}
+	
 }
 
 exit();
