a81e32dc03037ae7f3541fdbea0f7d6a6f6bb159
Tarako0329 <86461908+Tarako0329@users.noreply.github.com>
2025-03-27 10:20:00 +0900

    ver1.36.3:SNS自動投稿機能

--- super_admin.php
+++ super_admin.php
@@ -1,22 +1,145 @@
 <?php
+/*
+自動投稿の間隔設定（最短最長の間でランダム）
+自動投稿内容の履歴
+新規出店者作成
+
+*/
 require "php_header.php";
 
-$new_user_cd = 1234;
+$msg="";
+if(!empty($_POST["new_yagou"])){
+	$stmt = $pdo_h->prepare("select uid from Users where uid = :uid FOR UPDATE");
+	while(true){
+		//乱数からユーザIDを発行し、重複してなければ使用する
+		$new_id = rand(0,99999);
+		log_writer2("\$uid",$new_id,"lv3");
+		$stmt->bindValue("uid", $new_id, PDO::PARAM_INT);
+		$stmt->execute();
+		$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		if(empty($row["uid"])){
+			break;
+		}
+	}
+	//新規出店者登録SQL
+	try{
+		$pdo_h->beginTransaction();
+		$stmt = $pdo_h->prepare("INSERT INTO Users_online(uid,logo,yagou) values('$new_id','upload/logo_sample.png','".$_POST["new_yagou"]."') ");
+		$stmt->execute();
+		$stmt = $pdo_h->prepare($sql="INSERT INTO Users(uid,mail,password,question,answer) values('$new_id','-','-','-','-') ");
+		$stmt->execute();
+		$pdo_h->commit();
+		$msg='出店アカウントを作成しました。';
+	}catch(Exception $e){
+		$pdo_h->rollBack();
+		var_dump($e);
+		exit();
+	}
+}
 
-echo rot13encrypt2($new_user_cd)."<br><br>";
-
-
+if(!empty($_POST["sns_f"]) && !empty($_POST["sns_t"])){
+	//自動ツイート間隔幅設定
+	try{
+		$pdo_h->beginTransaction();
+		$stmt = $pdo_h->prepare("delete from online_shop_config ");
+		$stmt->execute();
+		$stmt = $pdo_h->prepare($sql="INSERT INTO online_shop_config(post_interval_F,post_interval_T) values(".$_POST["sns_f"].",".$_POST["sns_t"].") ");
+		$stmt->execute();
+		$pdo_h->commit();
+		$msg='自動ツイート間隔幅を設定しました。';
+	}catch(Exception $e){
+		$pdo_h->rollBack();
+		var_dump($e);
+		exit();
+	}
+}
 $sql = "select * from Users_online";
 $stmt = $pdo_h->prepare($sql);
 $stmt->execute();
 
 $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$i=0;
 foreach($data as $row){
-  echo $row["yagou"]."　　　<a href=admin_menu.php?key=".rot13encrypt2($row["uid"]).">管理サイトへ</a><br>";
+	 //$row["yagou"]."　　　<a href=admin_menu.php?key=".rot13encrypt2($row["uid"]).">管理サイトへ</a><br>";
+	 $data[$i] = ["uri" => "admin_menu.php?key=".rot13encrypt2($row["uid"]),"yagou" => $row["yagou"]];
+	 $i++;
 }
 
-//新規出店者登録SQL
-$sql="INSERT INTO Users_online(uid,logo) values('1234','upload/logo_sample.png') ";
-$sql="INSERT INTO Users(uid,mail,password,question,answer) values('1234','-','-','-','-') ";
 ?>
+<!DOCTYPE html>
+<html lang='ja'>
+<head prefix="og: http://ogp.me/ns#">
+		<?php 
+		//共通部分、bootstrap設定、フォントCND、ファビコン等
+		include "head_admin.php" 
+		?>
+		<style>
+			.btn{
+				min-width: 50px;
+			}
+		</style>
+		<TITLE><?php echo TITLE;?>管理画面</TITLE>
+</head>
+<BODY >
+	<div id='app' class='container'>
+		<header class='common_header alice-regular p-3 mb-3' style='color:brown;'>
+			<h3>システム管理画面</h3>
+		</header>
+		<main>
+			<div v-if='msg!==""' class='alert alert-success'>{{msg}}</div>
+			<div class='row'>
+				<div class='col-md-6'>
+					<form method="post" action='super_admin.php'>
+						<label class='form-label'>X.com</label>
+						<input type='text' v-model='sns_f' name='sns_f' class='form-control mb-1' required placeholder="つぶやき最短分" id='sns_f'>
+						<input type='text' v-model='sns_t' name='sns_t' class='form-control mb-3' required placeholder="つぶやき最長分">
+						<button type='submit' class='btn btn-primary'>設定</button>
+					</form>
+					<hr>
+				</div>
+				<div class='col-md-6'>
+					<h4 class='alice-regular'>新規出店登録</h4>
+					<form method="post" action='super_admin.php'>
+						<div class='row ps-3 pe-3'>
+							<input type='text' name='new_yagou' class='form-control mb-3' placeholder="屋号">
+							<button type='submit' class='btn btn-primary'>登録</button>
+						</div>
+					</form>
+					<hr>
+					<h4 class='alice-regular'>出店者管理</h4>
+					<template v-for='(list,index) in menu' :key='list.yagou'>
+						<a :href='list.uri' class='btn btn-info text-truncate m-2' style='width:100px;'>{{list.yagou}}</a>
+					</template>
+					<hr>
+				</div>
+			</div>
+		</main>
+		<footer></footer>
+	</div>
+	<script><!--vue-->
+		const { createApp, ref, onMounted, onBeforeMount, computed,watch } = Vue;
+		createApp({//管理者メニュー
+		 setup() {
+		   const menu = ref(
+		     <?php echo json_encode($data, JSON_UNESCAPED_UNICODE); ?>
+		   )
+			 const sns_f = ref(30)
+			 const sns_t = ref(60)
+			 const msg = ref('<?php echo $msg;?>')
+				
+		   onMounted(()=>{
+		     console_log(`onMounted`)
+		   })
+			
+		   return{
+		     menu,
+				 sns_f,
+				 sns_t,
+				 msg,
+		   }
+		 }
+		}).mount('#app');
+	</script>
+</BODY>
+</html>
---
+++ tweet_as_bot.php
@@ -0,0 +1,114 @@
+<?php
+//１日５０ツイートまでかつ月１５００ツイートまで
+if (php_sapi_name() != 'cli') {
+  exit('このスクリプトはCLIからのみ実行可能です。');
+}
+require "php_header.php";
+register_shutdown_function('shutdown_ajax',basename(__FILE__));
+log_writer2("\$_POST",$_POST,"lv1");
+use Abraham\TwitterOAuth\TwitterOAuth;
+use GeminiAPI\Client;
+use GeminiAPI\Resources\ModelName;
+use GeminiAPI\Resources\Parts\TextPart;
+
+$status="false";
+
+//実行時間check
+$stmt = $pdo_h->prepare("select * from online_shop_config");
+$stmt->execute();
+$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if($row["next_post_time"] >= date('Y-m-d H:i:s')){
+  //実行時間前
+  exit();
+}
+
+if(EXEC_MODE<>"Product"){
+    $msg = "ツイートが送信されました！";
+    $status = "success";
+}else{
+  //ポスト内容取得
+
+  //商品選定
+  $stmt = $pdo_h->prepare("select U.yagou,M.* from shouhinMS_online as M inner join Users_online as U on M.uid=U.uid where status=show and auto_sns_post not like '%X%' order by shouhinCD");
+  $stmt->execute();
+  $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $post_index = rand(0,(count($row)-1));
+
+  //つぶやき作成
+  $sns_type = !empty($_POST["sns_type"])?$_POST["sns_type"]:"SNS" ;
+  $uid = $row[$post_index]["uid"];
+  $shouhinCD = $row[$post_index]["shouhinCD"];
+  $yagou = $row[$post_index]["yagou"];
+  $hinmei = $row[$post_index]["shouhinNM"];
+  $sort_info = $row[$post_index]["sort_info"];
+  $information = $row[$post_index]["information"];
+  
+  $discription = "URL：".ROOT_URL."product.php?id=".$uid."-".$shouhinCD."&z= 販売元:".$yagou." 商品名：".$hinmei."。説明：".$sort_info." ".$information;
+  
+  
+  $client = new Client(GEMINI);
+  
+  $response = $client->withV1BetaVersion()
+      ->generativeModel(ModelName::GEMINI_1_5_FLASH)
+      ->withSystemInstruction('凄腕インフルエンサーとして')
+      ->generateContent(
+          //new TextPart('Xでバズるハッシュタグとハッシュタグを含めた日本語の投稿例を３つ、javascriptでそのまま使えるJSON形式で簡潔に提案してください。JSONの形式について、ハッシュタグはtags。投稿例はrei1,rei2,rei3で。'.$discription),
+          //new TextPart('Xでバズるハッシュタグを10個と,URL,ハッシュタグを含めた日本語の投稿例をXに投稿できる文字数内で３つをJSON形式{"posts":{"tags":[tag1,tag2], "texts":[紹介文1,紹介文2,紹介文3]}}で出力。'.$discription),
+          //new TextPart('Xでバズるハッシュタグを10個と,日本語の投稿例を３つJSON形式{"posts":{"tags":[tag1,tag2], "texts":[{text:"",tags:[...],URL:""}]}}で出力。投稿例は110文字程度でハッシュタグ不要。投稿例はtexts.textに格納。URLはtexts.URLに格納。ハッシュタグはtexts.tagsに格納。'.$discription),
+          new TextPart($sns_type.'でバズるハッシュタグを10個と,購買意欲を掻き立てる日本語の投稿例を３つJSON形式{"posts":{"tags":[tag1,tag2], "texts":[{text:"",tags:[...],URL:""}]}}で出力。投稿例は'.$sns_type.'文字程度でハッシュタグ不要。投稿例はtexts.textに格納。URLはtexts.URLに格納。ハッシュタグはtexts.tagsに格納。'.$discription),
+      );
+      
+  //print nl2br($response->text());
+      
+  $answer = $response->text();
+  $answer = str_replace('```json','',$answer);
+  $answer = str_replace('```','',$answer);
+  $answer = str_replace('\n','',$answer);
+  $answer = str_replace('\r','',$answer);
+  $answer = str_replace('\r\n','',$answer);
+  $answer = substr($answer,1);
+
+  
+  $text = $_POST["tweet"].$_POST["hash_tag"]." ".$_POST["URL"];
+  
+  
+  define("API_KEY",$_ENV["X_API_KEY"]);
+  define("API_SECRET_KEY",$_ENV["X_API_SECRET_KEY"]);
+  define("ACCESS_TOKEN",$_ENV["X_ACCESS_TOKEN"]);
+  define("SECRET_ACCESS_TOKEN",$_ENV["X_SECRET_ACCESS_TOKEN"]);
+  
+  try{
+    $connection = new TwitterOAuth(
+      API_KEY,
+      API_SECRET_KEY,
+      ACCESS_TOKEN,
+      SECRET_ACCESS_TOKEN
+    );
+    
+    $connection->setApiVersion('2');
+    
+    //URLが半角23文字扱い。ハッシュタグは含まない
+    $result = $connection->post("tweets", ["text"=>$text], ['jsonPayload'=>true]);
+  
+    $httpCode = $connection->getLastHttpCode();
+    
+    if ($httpCode == 201) { // 201は作成成功を示すステータスコード
+      //$this->info("ツイートが送信されました！");
+      $msg = "ツイートが送信されました！\n";
+      $status = "success";
+    } else {
+      $errorMessage = isset($result->errors) ?json_encode($result->errors, JSON_UNESCAPED_UNICODE) :'不明なエラー';
+      //$this->error("ツイートの送信に失敗しました。HTTPコード: $httpCode, エラーメッセージ: $errorMessage");
+      $msg = "ツイートの送信に失敗しました。HTTPコード: $httpCode, エラーメッセージ: $errorMessage ";
+      log_writer2("\$msg",$msg,"lv1");
+    }
+  }catch(Exception $e){
+    //print_r($e,true);
+    echo "catch(Exception \$e)";
+    log_writer2("\$e",$e,"lv0");
+  }
+}
+
+exit();
+?>
