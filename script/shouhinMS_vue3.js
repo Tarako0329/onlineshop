26cbdf6ff50fdb738e518cbe78fef4e3896d2f93
Tarako0329 <86461908+Tarako0329@users.noreply.github.com>
2025-05-22 15:53:24 +0900

    ver1.42.3:細かな修正

--- script/shouhinMS_vue3.js
+++ script/shouhinMS_vue3.js
@@ -525,7 +525,7 @@ const shouhinMS = (Where_to_use,p_token,p_hash) => createApp({//商品マスタ�
 			.then((response) => {
 				console_log('get_AI_seo succsess')
 				console_log(response.data.result)
-				AI_answer_seo.value = response
+				AI_answer_seo.value = response.data.result
 				document.getElementById('modalon_seo').click()
 			})
 			.catch((error)=>{
