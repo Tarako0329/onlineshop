64102ccbb32aa6fdc03fd033e0e1a540587cd975
Tarako0329 <86461908+Tarako0329@users.noreply.github.com>
2025-06-18 17:19:28 +0900

    ver1.43.2:GeminiAPIのresponseSchema設定(JSONを強制で返す)

--- script/shouhinMS_vue3.js
+++ script/shouhinMS_vue3.js
@@ -644,7 +644,7 @@ const shouhinMS = (Where_to_use,p_token,p_hash) => createApp({//商品マスタ�
 			params.append(`Article`, Article);
 			params.append(`type`, 'one');
 			params.append(`answer_type`, 'json')
-			params.append(`response_schema`, response_schema)
+			params.append(`response_schema`, JSON.stringify(response_schema))
 	
 			//GET_AI_SEO(shouhinNM.value,midasi.value,info.value)
 			axios.post("ajax_chk_gemini.php",params, {headers: {'Content-Type': 'multipart/form-data'}})
