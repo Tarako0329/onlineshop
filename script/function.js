8a211d0c244d2a503a1d553079eca6cf7708c7a8
Tarako0329 <86461908+Tarako0329@users.noreply.github.com>
2025-09-11 10:47:42 +0900

    ver1.47.1:購入者の足跡

--- script/function.js
+++ script/function.js
@@ -1,12 +1,11 @@
 //js 共通関数格納予定
 const console_log=(log,lv)=>{
   //lv:all=全環境 undefined=本番以外
-  //console.log(lv)
-  if(lv==="all"){
+  console.log(`lv:${lv}`)
+  
+  if(lv=="all"){
     console.log(log)
-  }/*else if(lv==="lv2" && KANKYO!=="Product"){
-    console.log(log)
-  }*/else if((lv==="lv3" || lv===undefined) && (KANKYO!=="Product")){
+  }else if((lv==="lv3" || lv===undefined) && (KANKYO!=="Product")){
     //console.log(KANKYO)
     console.log(log)
   }else{
