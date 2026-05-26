4f2160ff3760ad505b864575b086fd5210324dcb
Tarako0329 <86461908+Tarako0329@users.noreply.github.com>
2026-05-26 13:57:31 +0900

    ver1.67.0:月次アクセスログレポート

--- script/vue3.js
+++ script/vue3.js
@@ -63,8 +63,8 @@ const shops = (Where_to_use,p_token) => createApp({//サイト設定
       //GET_USER2()
       axios.get("ajax_get_ShopList.php")
       .then((response)=>{
-        console_log(response)
-        shoplist.value = response.Users_online
+        console_log(response.data)
+        shoplist.value = response.data.Users_online
       })
       document.getElementById("menu_Shops").classList.add("active");
     })
